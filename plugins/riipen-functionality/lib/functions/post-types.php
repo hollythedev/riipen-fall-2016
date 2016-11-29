8cb8b91ca010f09a75e4f539d2fe399ac1f0799a
Ringo <ringong95@gmail.com>
2016-11-29 10:33:13 -0800

    Added Organazations custom post type

--- plugins/riipen-functionality/lib/functions/post-types.php
+++ plugins/riipen-functionality/lib/functions/post-types.php
@@ -112,3 +112,56 @@ function register_Institutions_post_type() {
 
 }
 add_action( 'init', 'register_Institutions_post_type', 0 );
+
+// Register Custom Post Type
+function register_Organizationss_post_type() {
+
+	$labels = array(
+		'name'                  => 'Organizations',
+		'singular_name'         => 'Organizations Type',
+		'menu_name'             => 'Organizations',
+		'name_admin_bar'        => 'Organizations Type',
+		'archives'              => 'Organizations Archives',
+		'parent_item_colon'     => 'Organizations Item:',
+		'all_items'             => 'All Organizationss',
+		'add_new_item'          => 'Add New Organizationss',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Organizations',
+		'edit_item'             => 'Edit Organizations',
+		'update_item'           => 'Update Organizations',
+		'view_item'             => 'View Organizations',
+		'search_items'          => 'Search Organizations',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Organizations',
+		'uploaded_to_this_item' => 'Uploaded to this Organizations',
+		'items_list'            => 'Organizationss list',
+		'items_list_navigation' => 'Organizationss list navigation',
+		'filter_items_list'     => 'Filter Organizationss list',
+	);
+	$args = array(
+		'label'                 => 'Organizations Type',
+		'description'           => 'The summery page of Organizationss',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'Organizationss', $args );
+
+}
+add_action( 'init', 'register_Organizationss_post_type', 0 );
