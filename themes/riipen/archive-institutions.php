213a01bf691fddeaad3000a6fd330f3727d699e4
Ringo <ringong95@gmail.com>
2016-11-29 14:41:50 -0800

    Archive instituions pull correct data, and created style page for it

---
+++ themes/riipen/archive-institutions.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				
+			</header><!-- .page-header -->
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<a href="<?php echo the_permalink(); ?>">
+				<?php if ( has_post_thumbnail() ) : ?>
+					<?php the_post_thumbnail( 'large' ); ?>
+				<?php endif; ?>
+				</a>
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
