7ce47e4a0657c6a8f97466715227b963cc6e8aa6
Jonathan Funk <jonfunk88@hotmail.com>
2016-11-28 20:13:28 -0800

    more sections added to front page

--- themes/riipen/front-page.php
+++ themes/riipen/front-page.php
@@ -9,6 +9,20 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+      <section class="hero">
+      </section>
+      <section class="stats">
+      </section>
+      <section class="about">
+      </section>
+      <section class="difference">
+      </section>
+      <section class="testimonial">
+      </section>
+      <section class="school-carousel">
+      </section>
+      <section class="companies-carousel">
+      </section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
