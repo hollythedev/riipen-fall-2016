0f785743537f8c44d8966d0e0d9abb4c64164ae9
Ringo <ringong95@gmail.com>
2016-11-29 13:18:33 -0800

    Removed unused sidebar

--- themes/riipen/functions.php
+++ themes/riipen/functions.php
@@ -66,19 +66,7 @@ add_action( 'after_setup_theme', 'riipen_content_width', 0 );
 
 
 
-function riipen_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html( 'Sidebar' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-	
-}
-add_action( 'widgets_init', 'riipen_widgets_init' );
+
 
 function riipen_slider_widgets_init() {
 	register_sidebar( array(
