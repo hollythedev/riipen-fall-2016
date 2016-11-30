749aad21c1944380e95d856dba1db3e6de42519e
Ringo <ringong95@gmail.com>
2016-11-30 13:59:54 -0800

    Started list view buttons

---
+++ themes/riipen/archive-organizations.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="view-type-buttons">
+			<a class="list-view psudeo-button" > <div class="psudeo-button"></div></a>
+			<a class="box-view psudeo-button"> <div class="psudeo-button"></div> </a>
+		</div>
+		<div class="riipen-list">
+			<?php if ( have_posts() ) : ?>
+
+				<header class="page-header">
+
+				</header><!-- .page-header -->
+				
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<div class="personwrapper">
+						<a href="<?php echo the_permalink(); ?>">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<?php the_post_thumbnail( 'large' ); ?>
+							<?php endif; ?>
+						</a>
+					</div>
+				<?php endwhile; ?>
+
+				<?php the_posts_navigation(); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+			<?php endif; ?>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
