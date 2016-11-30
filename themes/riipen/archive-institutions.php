fd794ea08ed9b20ddd72d810e4937cb2af4617f4
Ringo <ringong95@gmail.com>
2016-11-30 10:08:25 -0800

    Added List links and added a uppercase helper class

--- themes/riipen/archive-institutions.php
+++ themes/riipen/archive-institutions.php
@@ -9,31 +9,35 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+		<div class="tabbingsection"> 
+			<a href="google.com" class="uppercase">Institutions</a>
+			<a href="Google.com" class="uppercase">Educators</a>
+		</div>
+		<div class="riipen-list">
+			<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
+				<header class="page-header">
 
-			<header class="page-header">
-				
-			</header><!-- .page-header -->
+				</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<a href="<?php echo the_permalink(); ?>">
-				<?php if ( has_post_thumbnail() ) : ?>
-					<?php the_post_thumbnail( 'large' ); ?>
-				<?php endif; ?>
-				</a>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<a href="<?php echo the_permalink(); ?>">
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'large' ); ?>
+						<?php endif; ?>
+					</a>
 
-			<?php endwhile; ?>
+				<?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
+				<?php the_posts_navigation(); ?>
 
-		<?php else : ?>
+			<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-		<?php endif; ?>
-
+			<?php endif; ?>
+		</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
