d3bbf328a88f6c4f295f515446f4000c9d69d637
Jonathan Funk <jonfunk88@hotmail.com>
2016-12-01 15:09:53 -0800

    fixed carousel headers

--- themes/riipen/js/scripts.js
+++ themes/riipen/js/scripts.js
@@ -23,12 +23,16 @@
  });
 
  //This will look for the iis in Riipen and give them a span
- $('.riipen-brand, #wp_posts_carousel-2').html(function (i, html) {
-   return html.replace('Riipen', '<span class="highlight-orange">R<span class="highlight-blue">ii</span>pen</span>');
+ $('.riipen-brand').html(function (i, html) {
+   return html.replace('Riipen ', '<span class="highlight-orange">R<span class="highlight-blue">ii</span>pen</span> ');
+ });
+
+ $('#wp_posts_carousel-2').html(function (i, html) {
+   return html.replace('Riipened', '<span class="highlight-orange">R<span class="highlight-blue">ii</span>pened</span>');
  });
 
  $('#wp_posts_carousel-3').html(function (i, html) {
-   return html.replace('Riipen', '<span class="highlight-blue">R<span class="highlight-orange">ii</span>pen</span>');
+   return html.replace('Riipened', '<span class="highlight-blue">R<span class="highlight-orange">ii</span>pened</span>');
  });
 
 })( jQuery );
