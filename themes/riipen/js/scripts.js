98e376e87ed77834f4ff2d92998afc3a6570aeac
Jonathan Funk <jonfunk88@hotmail.com>
2016-12-01 10:52:01 -0800

    fixed scripts file

--- themes/riipen/js/scripts.js
+++ themes/riipen/js/scripts.js
@@ -1,9 +1,7 @@
-
-		(function( $ ) {
+(function( $ ) {
 
  //This will show/hide the navigation list in mobile view.
-
- $('.menu-toggle').on('click', function(){
+ /*$('.menu-toggle').on('click', function(){
    event.preventDefault();
    $('#primary-menu').toggle();
   });
@@ -13,16 +11,24 @@
     $('#primary-menu').hide();
    });
 
-]
- $('.riipen-brand').html(function (i, html) {
- 	console.log("Is it working?");
- 	return html.replace('ii', '<span class="highlight">ii</span>');
-] $('.riipen-brand, #wp_posts_carousel-2').html(function (i, html) {
+ //This will show/hide the search form in navigation.
+ $('.show-search').on('click', function(event){
+   event.preventDefault();
+   $('.site-search input[type=search]').slideToggle("fast").css('display','block').focus();
+ });
+
+ //This will hide the search form when no longer in focus.
+ $('.site-search input[type=search]').on('focusout', function(){
+   $(this).slideToggle("fast");
+ });
+
+ //This will look for the iis in Riipen and give them a span
+ $('.riipen-brand, #wp_posts_carousel-2').html(function (i, html) {
    return html.replace('Riipen', '<span class="highlight-orange">R<span class="highlight-blue">ii</span>pen</span>');
  });
 
  $('#wp_posts_carousel-3').html(function (i, html) {
    return html.replace('Riipen', '<span class="highlight-blue">R<span class="highlight-orange">ii</span>pen</span>');
- });
+ });*/
 
 })( jQuery );
