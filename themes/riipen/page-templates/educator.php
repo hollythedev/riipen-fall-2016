d4b7c2423fb9bffc88dc9d4d302ff9921b34b0dd
Holly <papiniu@outlook.com>
2016-12-01 16:52:07 -0800

    Added picture fields

--- themes/riipen/page-templates/educator.php
+++ themes/riipen/page-templates/educator.php
@@ -5,13 +5,15 @@
     <p> <?php echo CFS()->get( 'motto'); ?> </p>
 </div>
 
-<div class="educator-talent-section-2"> </div>
+<div class="educator-talent-section-2"> 
+<?php echo CFS()->get( 'banner'); ?></div>
 <div class="educator-talent-section-3">
     <p> 4 easy steps to being Riipened </p>
     <a href="<?php the_permalink(); ?>" class='eductor-talent-btn'> riipen up</a>
 </div>
 
-<div class="educator--talentsection-4"> </div>
+<div class="educator--talentsection-4"> 
+<?php echo CFS()->get( 'banner2'); ?></div>
 
 <div class="educator-talent-section-5">
     <?php echo CFS()->get( 'testimonial'); ?>
