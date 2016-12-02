0c5abd4ae7c98b7c4805206d6009169c7cb0576a
Ringo <ringong95@gmail.com>
2016-12-02 14:05:49 -0800

    Added tabs and tab styling

--- themes/riipen/js/tabs.js
+++ themes/riipen/js/tabs.js
@@ -3,9 +3,7 @@ jQuery(document).ready(function() {
 		var currentAttrValue = jQuery(this).attr('href');
 		console.log(currentAttrValue);
         // Show/Hide Tabs
-        // jQuery('.tabs ' + currentAttrValue).siblings().slideUp(400);
-        // jQuery('.tabs ' + currentAttrValue).delay(400).slideDown(400);
-        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+     	jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
 
         // Change/remove current tab to active
         jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
--- themes/riipen/single-organizations.php
+++ themes/riipen/single-organizations.php
@@ -36,31 +36,47 @@ get_header(); ?>
 			<?php endwhile; // End of the loop. ?>
 			<div class="tabs">
 				<ul class="tab-links">
-					<li class="active"><a href="#tab1">Tab #1</a></li>
-					<li><a href="#tab2">Tab #2</a></li>
-					<li><a href="#tab3">Tab #3</a></li>
-					<li><a href="#tab4">Tab #4</a></li>
+					<li class="active">
+						<a href="#tab1">
+							<?php echo CFS()->get( 'first_tab_name' ); ?>	
+						</a>
+					</li>
+					<li>
+						<a href="#tab2">
+							<?php echo CFS()->get( 'second_tab_name' ); ?>
+						</a>
+					</li>
+					<li>
+						<a href="#tab3">
+							<?php echo CFS()->get( 'third_tab_name' ); ?>
+						</a>
+					</li>
+					<li>
+						<a href="#tab4">
+							<?php echo CFS()->get( 'fourth_tab_name' ); ?>
+						</a>
+					</li>
 				</ul>
-				
+
 				<div class="tab-content">
 					<div id="tab1" class="tab active">
-						<p>Tab #1 content goes here!</p>
-						<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis.</p>
+						<h3><?php echo CFS()->get( 'first_tab_title' ); ?></h3>
+						<p><?php echo CFS()->get( 'first_tab_content' ); ?></p>
 					</div>
-					
+
 					<div id="tab2" class="tab">
-						<p>Tab #2 content goes here!</p>
-						<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis. In hac habitasse platea dictumst. Ut euismod tempus hendrerit. Morbi ut adipiscing nisi. Etiam rutrum sodales gravida! Aliquam tellus orci, iaculis vel.</p>
+						<h3><?php echo CFS()->get( 'second_tab_title' ); ?></h3>
+						<p><?php echo CFS()->get( 'second_tab_content' ); ?></p>
 					</div>
-					
+
 					<div id="tab3" class="tab">
-						<p>Tab #3 content goes here!</p>
-						<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum ri.</p>
+						<h3><?php echo CFS()->get( 'third_tab_title' ); ?></h3>
+						<p><?php echo CFS()->get( 'third_tab_content' ); ?></p>
 					</div>
-					
+
 					<div id="tab4" class="tab">
-						<p>Tab #4 content goes here!</p>
-						<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis. In hac habitasse platea dictumst. Ut euismod tempus hendrerit. Morbi ut adipiscing nisi. Etiam rutrum sodales gravida! Aliquam tellus orci, iaculis vel.</p>
+						<h3><?php echo CFS()->get( 'fourth_tab_title' ); ?></h3>
+						<p><?php echo CFS()->get( 'fourth_tab_content' ); ?></p>
 					</div>
 				</div>
 			</div>
