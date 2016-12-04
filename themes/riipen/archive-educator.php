c786df2d9ab6653c60112df1a6eb934d6974866c
Ringo <ringong95@gmail.com>
2016-12-03 16:07:27 -0800

    Changed how educators images are displayed

--- themes/riipen/archive-educator.php
+++ themes/riipen/archive-educator.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-<button onclick="history.go(-1);" class="back"><  Back</button>
+	<button onclick="history.go(-1);" class="back"><  Back</button>
 	<main id="main" class="site-main" role="main">
 		<div class="tabbingsection">
 
@@ -27,29 +27,33 @@ get_header(); ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 					<div class="personwrapper">
-					<div>
-					<a href="<?php echo the_permalink(); ?>">
-						<?php if ( has_post_thumbnail() ) : ?>
-							<?php the_post_thumbnail( 'large' ); ?>
-						<?php endif; ?>
-					</a>
-					</div>
-					<?php the_title( sprintf( '<h2 class="entry-title uppercase educatorname"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-					<p class="educators-category educatorname uppercase"> 
-					
-						 <?php the_tags("" ); ?> 
-					</div>
-				<?php endwhile; ?>
+						<div>
+							<a href="<?php the_permalink(); ?>" rel="bookmark"> 
+								<?php if ( has_post_thumbnail() ) : ?>
+									<?php $url = wp_get_attachment_url( get_post_thumbnail_id() ) ?>
+									
+									<div class="educator-image" style=background-image:url("<?php echo $url ?>") no repeat ;>
+								
+									</div>
+								</a>
+							<?php endif ?>
+						</div>
+						<?php the_title( sprintf( '<h2 class="entry-title uppercase educatorname"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+						<p class="educators-category educatorname uppercase"> 
 
-				<?php the_posts_navigation(); ?>
+							<?php the_tags("" ); ?> 
+						</div>
+					<?php endwhile; ?>
 
-			<?php else : ?>
+					<?php the_posts_navigation(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+				<?php else : ?>
 
-			<?php endif; ?>
-		</div>
-	</main><!-- #main -->
-</div><!-- #primary -->
+					<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-<?php get_footer(); ?>
+				<?php endif; ?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	<?php get_footer(); ?>
