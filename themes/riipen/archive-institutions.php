9ce620e0a3a2cd643990e43541376059e60a5082
Ringo <ringong95@gmail.com>
2016-12-05 11:29:23 -0800

    Touched up styling on archive pages

--- themes/riipen/archive-institutions.php
+++ themes/riipen/archive-institutions.php
@@ -8,14 +8,16 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-<button onclick="history.go(-1);" class="back"><  Back</button>
+	<button onclick="history.go(-1);" class="back"><  Back</button>
 	<main id="main" class="site-main" role="main">
-		<div class="tabbingsection">
+	<div  class="tabbingwrapper">
+			<div class="tabbingsection">
 
-			<a class="inactive uppercase">Institutions</a>
+				<a class="inactive uppercase">Institutions</a>
 
-			<a class="active uppercase" href="<?php echo get_post_type_archive_link( 'educator' ); ?>">Educators</a>
+				<a class="active uppercase" href="<?php echo get_post_type_archive_link( 'educator' ); ?>">Educators</a>
 
+			</div>
 		</div>
 		<div class="riipen-list">
 			<?php if ( have_posts() ) : ?>
@@ -27,12 +29,12 @@ get_header(); ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 					<div class="personwrapper">
-					<a href="<?php echo the_permalink(); ?>">
-						<?php if ( has_post_thumbnail() ) : ?>
-							<?php the_post_thumbnail( 'large' ); ?>
-						<?php endif; ?>
-					</a>
-			</div>
+						<a href="<?php echo the_permalink(); ?>">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<?php the_post_thumbnail( 'large' ); ?>
+							<?php endif; ?>
+						</a>
+					</div>
 				<?php endwhile; ?>
 
 				<?php the_posts_navigation(); ?>
