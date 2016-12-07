e6924969e86041243c24b4e3b166049a30ac4b04
Holly <papiniu@outlook.com>
2016-12-07 14:50:56 -0800

    Added picture to educator section div 4, styled it, set margins for button

--- themes/riipen/page-templates/educator.php
+++ themes/riipen/page-templates/educator.php
@@ -22,7 +22,7 @@
                 <a href="<?php the_permalink(); ?>" class='educator-talent-btn'> riipen up</a>
             </div>
             <div class="educator-talent-section-4">
-                <?php echo CFS()->get( 'banner_2'); ?>
+                
             </div>
             <div class="educator-talent-section-5">
                 <p class="testimonial">
