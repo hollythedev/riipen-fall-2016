3b51f99227a84e8bc3c20676424077b2bb971b0d
Holly <papiniu@outlook.com>
2016-12-07 19:22:45 -0800

    Added circles to desktop educator page

--- themes/riipen/page-templates/educator.php
+++ themes/riipen/page-templates/educator.php
@@ -17,40 +17,64 @@
             </div>
             <div class="educator-talent-section-2">
             </div>
-            <div class= "desktop-green"> 
-                <div class= "desktop-green-container1">
-                <h2>R<span>ii</span>pen</h2><p> your assignments</p>
+            <section class="desktop-green">
+                <div class="desktop-green-container1">
+                    <h2>R<span>ii</span>pen</h2>
+                    <p> your assignments</p>
                 </div>
                 <div class="desktop-green-container2">
-                <div class="left">
-                    <p>Leverage your students work and add industry value to the academic curriculum</p>
+                    <div class="left">
+                        <p>Leverage your students work and add industry value to the academic curriculum</p>
+                    </div>
+                    <div class="middle">
+                        <p>Riipen matches your projects desired learning outcomes to industry partners that share an interest
+                            in those skills, who in turn validate students success</p>
+                    </div>
+                    <div class="right">
+                        <p>It starts with developing an assignment that meets industry standards</p>
+                    </div>
                 </div>
-                <div class="middle">
-                    <p>Riipen matches your projects desired learning outcomes to industry partners that share an interest in those skills, who in turn validate students success</p>
-                </div>
-                <div class="right">
-                    <p>It starts with developing an assignment that meets industry standards</p>
-                </div>
-                </div>
-                 </div>
-                 <div class= "desktop-circles">
+            </section>
+            <section class="desktop-circles">
+				
+						<div class="form">
+							<h2>1</h2>
+                             <div class= "circle-text">
+							<p>Fill out a form</p>
+                            <p>Think about learning outcomes that you want for this project</p>
+						</div>
+                        </div>
+					
+						<div class="upload">
+							<h2>2</h2>
+                             <div class= "circle-text">
+							<p> Upload your assignment</p>
+                            <p> Fill in the form on our platform and press submit</p>
+						</div>
+                        </div>
+					
+						<div class="answer">
+							<h2>3</h2>
+                            <div class= "circle-text">
+							<p>Answer your phone</p>
+                            <p>Once we have this information, we will call you within 48 hours.</p>
+						</div>
+                        </div>
+            </section>
 
-                 </div>
             <div class="educator-talent-section-3">
                 <p> 4 easy steps to being Riipened </p>
                 <a href="<?php the_permalink(); ?>" class='educator-talent-btn'> riipen up</a>
             </div>
             <div class="educator-talent-section-4">
-                
             </div>
             <div class="educator-talent-section-5">
                 <p class="testimonial">
                     <?php echo CFS()->get( 'testimonial'); ?>
-     
-                        <p class="position">
-                            <?php echo CFS()->get( 'position'); ?> </p>
-                        <p class="name">
-                            <?php echo CFS()->get( 'name'); ?> </p>
+                    <p class="position">
+                        <?php echo CFS()->get( 'position'); ?> </p>
+                    <p class="name">
+                        <?php echo CFS()->get( 'name'); ?> </p>
             </div>
         </section>
         <?php dynamic_sidebar( 'carousels' ); ?>
