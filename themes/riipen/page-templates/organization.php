39d3880473da9c04f73478d4af49a2cab164bc68
Holly <papiniu@outlook.com>
2016-12-09 10:04:27 -0800

    Created organization page, changed ipad photo of it

--- themes/riipen/page-templates/organization.php
+++ themes/riipen/page-templates/organization.php
@@ -1,34 +1,61 @@
-<?php /* Template Name: Organiazation */ ?>
+<?php /* Template Name: Educator */ ?>
 <?php get_header(); ?>
 <div id="primary" class="content-area">
-    <button onclick="history.go(-1);" class="back">&lt; Back</button>
+    <button onclick="history.go(-1);" class="back"> <i class="fa fa-arrow-left" aria-hidden="true"></i>  Back</button>
     <main id="main" class="site-main" role="main">
-        <section class="organization-page">
-            <div class="organization-section-1">
-                <h1>r<span>ii</span>pen</h1>
-                <p>
-                    <?php echo CFS()->get( 'motto'); ?> </p>
+        <section class="educator-page">
+            <div class="educator-talent-section-1">
+                <div class="circle">
+                    <h2 class="title">
+                        <?php the_title();?>
+                    </h2>
+                </div>
+                <div class="text-box">
+                    <h1>r<span>ii</span>pen</h1>
+                    <?php echo CFS()->get( 'motto'); ?>
+                </div>
             </div>
-            <div class="description">
-             <h3>r<span>ii</span>pen</h3> <p> your assignments </p>
-                <?php echo CFS()->get( 'description'); ?>
+            <div class="educator-talent-section-2">
+              <img src="<?php echo get_template_directory_uri() . '/images/journeymap.png'; ?>" alt="Journey Map" />
             </div>
-            <div class="organization-section-2">
-                <?php echo CFS()->get( 'banner'); ?>
-            </div>
-            <div class="organization-section-3">
+            <section class="desktop-green">
+                <div class="desktop-green-container1">
+                    <h2>R<span>ii</span>pen</h2>
+                    <p> your assignments</p>
+                </div>
+                <div class="desktop-green-container2">
+                    <div class="left">
+                        <p>Leverage your students work and add industry value to the academic curriculum</p>
+                    </div>
+                    <div class="middle">
+                        <p>Riipen matches your projects desired learning outcomes to industry partners that share an interest
+                            in those skills, who in turn validate students success</p>
+                    </div>
+                    <div class="right">
+                        <p>It starts with developing an assignment that meets industry standards</p>
+                    </div>
+                </div>
+            </section>
+
+            <section class="educator-talent-section-3">
                 <p> 4 easy steps to being Riipened </p>
-                <a href="<?php the_permalink(); ?>" class='eductor-talent-btn'> riipen up</a>
-            </div>
-            <div class="organizationsection-4">
-                <?php echo CFS()->get( 'banner_2'); ?>
-            </div>
-            <div class="organization-section-5">
-                <?php echo CFS()->get( 'testimonial'); ?>
-                <?php echo CFS()->get( 'position'); ?>
-                <?php echo CFS()->get( 'name'); ?>
-            </div>
+                <a href="<?php the_permalink(); ?>" class="educator-talent-btn"> riipen up</a>
+            </section>
+            <section class="educator-talent-section-4">
+                 <img src="<?php echo get_template_directory_uri() . '/images/organizationipad.jpg'; ?>" alt="Photo of iPad" />
+            </section>
+            <section class="educator-talent-section-5">
+                <div class= "text-container">   
+                <p class="testimonial">
+                    <?php echo CFS()->get( 'testimonial'); ?>
+                    <p class="position">
+                       - <?php echo CFS()->get( 'position'); ?> </p>
+                    <p class="name">
+                        <?php echo CFS()->get( 'name'); ?> </p>
+           </div>
+            </section>
         </section>
+        <?php dynamic_sidebar( 'carousels' ); ?>
     </main>
 </div>
 <!-- #primary -->
