911e1cdf6aa8f544362591c254759b169fc2e544
Jonathan Funk <jonfunk88@hotmail.com>
2016-12-09 14:07:55 -0800

    centered journey map

--- themes/riipen/page-templates/educator.php
+++ themes/riipen/page-templates/educator.php
@@ -33,11 +33,10 @@
                     </div>
                 </div>
             </section>
-
-<img src="<?php echo get_template_directory_uri() . '/images/journeymap.png'; ?>" class="normal" alt="Journey Map" />
+            <section class="journey-map">
+              <img src="<?php echo get_template_directory_uri() . '/images/journeymap.png'; ?>" class="normal" alt="Journey Map" />
               <img src="<?php echo get_template_directory_uri() . '/images/sidewaysjourneymap.png'; ?>" class= "sideways" alt="Journey Map" />
-
-
+            </section>
             <section class="educator-talent-section-3">
                 <p> 4 easy steps to being Riipened </p>
                 <a href="<?php the_permalink(); ?>" class="educator-talent-btn"> riipen up</a>
@@ -46,7 +45,7 @@
                  <img src="<?php echo get_template_directory_uri() . '/images/educatoripad.jpg'; ?>" alt="Photo of iPad" />
             </section>
             <section class="educator-talent-section-5">
-                <div class= "text-container">   
+                <div class= "text-container">
                 <p class="testimonial">
                     <?php echo CFS()->get( 'testimonial'); ?> </p>
                     <p class="position">
