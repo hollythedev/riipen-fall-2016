61c7d7113f0513bfd264b5c0009d1deff3d7830f
Holly <papiniu@outlook.com>
2016-12-11 16:49:15 -0800

    made single educator page input dynamic in backend

--- plugins/riipen-functionality/lib/functions/post-types.php
+++ plugins/riipen-functionality/lib/functions/post-types.php
@@ -40,7 +40,7 @@ function register_educators_post_type() {
 		'label'                 => 'Educator',
 		'description'           => 'Educators post',
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'thumbnail', ),
+		'supports'              => array( 'title','editor', 'thumbnail', ),
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
