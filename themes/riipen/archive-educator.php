84f916089315b5d90563f3ab6849e775370874e3
Ringo <ringong95@gmail.com>
2016-12-12 15:52:35 -0800

    refactored educator grid code

--- themes/riipen/archive-educator.php
+++ themes/riipen/archive-educator.php
@@ -10,8 +10,8 @@ get_header(); ?>
 <div id="primary" class="content-area ">
 
 	<main id="main" class="site-main" role="main">
-	<button onclick="history.go(-1);" class="back"><i class="fa fa-arrow-left" aria-hidden="true"></i>  Back</button>
-	<div class="hero-banner">
+		<button onclick="history.go(-1);" class="back"><i class="fa fa-arrow-left" aria-hidden="true"></i>  Back</button>
+		<div class="hero-banner">
 			<h1 class="uppercase banner-text">Where <span class="highlight-orange">talent</span> and <span class="highlight-orange">industry</span> meet</h1>
 		</div>
 		<div  class="tabbing-wrapper">
@@ -24,44 +24,43 @@ get_header(); ?>
 			</div>
 		</div>
 		<div class="riipen-list container">
+
 			<?php if ( have_posts() ) : ?>
+				<div class="company-wrapper">
 
-				<header class="page-header">
-
-				</header><!-- .page-header -->
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-					<div class="person-wrapper">
-
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<div class="person-wrapper">
+							
 							<a class="picture-wrapper" href="<?php the_permalink(); ?>" rel="bookmark">
 								<?php if ( has_post_thumbnail() ) : ?>
 									<?php $url = wp_get_attachment_url( get_post_thumbnail_id() ) ?>
 									<div class="image-wrapper">
-									<div class="educator-image" style=background-image:url("<?php echo $url ?>");>
+										<div class="educator-image" style=background-image:url("<?php echo $url ?>");>
 
-									</div>
+										</div>
 									</div>
 								</a>
 
 							<?php endif ?>
 
-						<?php the_title( sprintf( '<h2 class="entry-title uppercase educatorname"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-						<p class="educators-category educatorname uppercase">
+							<?php the_title( sprintf( '<h2 class="entry-title uppercase educatorname"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+							<p class="educators-category educatorname uppercase">
 
-							 <?php echo get_the_term_list( $post->ID , 'subject', "" , " ") ?>
+								<?php echo get_the_term_list( $post->ID , 'subject', "" , " ") ?>
+								
+							</div>
+						<?php endwhile; ?>
+						<div class="class_nav">
+							<?php posts_nav_link( " ", "Previous", "More" ); ?> 
 						</div>
-					<?php endwhile; ?>
-					<div class="class_nav">
-					<?php posts_nav_link( " ", "Previous", "More" ); ?> 
-					</div>
-				<?php else : ?>
+					<?php else : ?>
 
-					<?php get_template_part( 'template-parts/content', 'none' ); ?>
+						<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-				<?php endif; ?>
-			</div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					<?php endif; ?>
+				</div>
+			</main><!-- #main -->
+		</div><!-- #primary -->
 
-	<?php get_footer(); ?>
+		<?php get_footer(); ?>
